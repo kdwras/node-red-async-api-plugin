883deba796337985c3aeb18326fd97ed99d9c3e7
tr1m <k.doras@spectre.gr>
2025-04-12 23:22:55 +0300

    ++

--- nodered/nodes/async-api-node/routes/router.js
+++ nodered/nodes/async-api-node/routes/router.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const fs = require("fs");
-const path = require('path');
-const multer = require("multer");
-const getParsedAsyncApiFile = require("../async-api-parser/async-api-parser");
-const mime = require("mime-types");
 
 module.exports = (RED) => {
+
+    const express = require("express");
+    const fs = require("fs");
+    const Providers = require("../providers/providers");
+    const Utils = require("../utils/utils")(RED);
 
     const router = express.Router();
 
@@ -22,15 +21,15 @@ module.exports = (RED) => {
         }
 
         try {
-            const filePath = getFilePath(nodeId);
-            const file = await fetchFile(filePath);
+            const filePath = Utils.getFilePath(nodeId);
+            const file = await Utils.fetchFile(filePath);
             const fileContent = file.fileContent;
 
             if (!fileContent) {
                 return res.status(400).json({error: "No file content provided"});
             }
 
-            const data = await getParsedAsyncApiFile(fileContent);
+            const data = await Utils.getParsedAsyncApiFile(fileContent);
             const document = data.document;
 
             const servers = [];
@@ -109,14 +108,14 @@ module.exports = (RED) => {
             res.status(404).json({error: "Node not found!"});
         }
 
-        const fileDest = getFilePath(nodeId);
+        const fileDest = Utils.getFilePath(RED, nodeId);
 
         if (!fs.existsSync(fileDest)) {
             return res.status(404).json({error: "No uploaded files found"});
         }
 
         try {
-            const file = await fetchFile(fileDest);
+            const file = await Utils.fetchFile(fileDest);
             res.json(file);
         } catch (error) {
             res.status(500).json({error: error});
@@ -153,79 +152,31 @@ module.exports = (RED) => {
 
     }
 
-    /** private functions **/
-    function getFileProvider() {
-        const storage = multer.diskStorage({
-            destination: (req, file, cb) => {
-                const {nodeId} = req.params;  // Get node ID from URL
+    function connectToServer(req, res) {
+        const {nodeId} = req.params;
+        const node = RED.nodes.getNode(nodeId);
 
-                if (!nodeId) {
-                    return cb(new Error("Missing nodeId"), null);
-                }
+        if (!node) {
+            return res.status(404).json({error: "Node not found"});
+        }
 
-                const projectFolder = getFilePath(nodeId);
-                // Ensure the folder exists
-                if (!fs.existsSync(projectFolder)) {
-                    fs.mkdirSync(projectFolder, {recursive: true});
-                }
-
-                // Set the destination folder for uploads
-                cb(null, projectFolder);
-            },
-            filename: (req, file, cb) => {
-                // Set the filename to be the original name of the file
-                cb(null, file.originalname);
-            },
-        });
-        //Multer provides a middleware (upload.single("file")) that extracts the file from the request, processes it, and stores it in a defined location.
-        return multer({storage});
+        try {
+            Utils.connectToServer(node);
+            res.status(200).json({message: "MQTT connection initiated"});
+        } catch (err) {
+            res.status(500).json({error: err.message});
+        }
     }
 
-    function getFilePath(nodeId) {
-        const userDir = RED.settings.userDir;
-        const projects = RED.settings.get("projects");
-
-        const basePath = projects?.activeProject
-            ? path.join(userDir, "projects", projects.activeProject, "uploads")
-            : path.join(userDir, "uploads");
-
-        return path.join(basePath, nodeId);
-    }
-
-    function fetchFile(uri) {
-        return new Promise((resolve, reject) => {
-            fs.readdir(uri, (err, files) => {
-                if (err || files.length === 0) {
-                    reject(new Error("No saved file found"));
-                    return;
-                }
-
-                const latestFile = files[0];
-                const filePath = path.join(uri, latestFile); // Get the first file
-                const fileType = mime.lookup(latestFile); // Get MIME type
-
-                fs.readFile(filePath, "utf8", (err, data) => {
-                    if (err) {
-                        reject(new Error('Could not read file'));
-                        return;
-                    }
-                    resolve({
-                        fileContent: data,
-                        fileName: files[0],
-                        fileType: fileType
-                    });
-                });
-            });
-        });
-    }
 
     function initRoutes() {
         // Assign handlers to routes
         router.get("/async-api-red/:nodeId/data", getData);
-        router.post("/async-api-red/:nodeId/file", getFileProvider().single("file"), uploadFile);
+        router.post("/async-api-red/:nodeId/file", Providers.getFile().single("file"), uploadFile);
         router.get("/async-api-red/:nodeId/file", getFile);
         router.get("/async-api-red/:nodeId/user-selections", getUserSelections);
         router.post("/async-api-red/:nodeId/user-selections", saveUserSelections);
+        router.post("/async-api-red/:nodeId/server-connect", connectToServer);
     }
 
     return router;
---
+++ nodered/nodes/async-api-node/utils/utils.js
@@ -0,0 +1,129 @@
+const {Parser: Utils} = require('@asyncapi/parser');
+const fs = require('fs');
+const mqtt = require("mqtt");
+const path = require("path");
+const mime = require("mime-types");
+
+module.exports = (RED) => {
+    async function getParsedAsyncApiFile(data) {
+
+        // Read the AsyncAPI file
+
+        try {
+            const parser = new Utils();
+
+            const errors = await parser.validate(data);
+
+            if (errors.length) {
+                console.error('❌ Validation errors found:');
+                errors.forEach((error, index) => {
+                    console.error(`${index + 1}. ${error.message} (at ${error.location.pointer})`);
+                });
+                return;
+            }
+
+            const ret = await parser.parse(data);
+
+            console.log('✅ AsyncAPI document is valid!');
+            //  console.log('Parsed Document:', ret);
+
+            return ret;
+
+        } catch (error) {
+            console.error('Error reading or parsing the file:', error);
+        }
+    }
+
+    function connectToServer(node) {
+
+        //locally srv ----> mqtt://172.17.0.1:1883
+        node.serverUrl = node.context().get("serverUrl");
+        node.topic = node.context().get("topic");
+
+        if (!node.serverUrl || !node.topic) {
+            node.error("MQTT Server URL or Topic is missing!");
+            node.status({fill: "red", shape: "ring", text: "Missing MQTT Config"});
+            return;
+        }
+
+        node.status({fill: "yellow", shape: "ring", text: "Connecting..."});
+
+        const options = {
+            connectTimeout: 5000,  // Wait for 5 seconds before timeout
+            reconnectPeriod: 2000  // Try to reconnect every 2 seconds
+        };
+
+        // Connect to MQTT broker
+        const client = mqtt.connect(node.serverUrl, options);
+
+        client.on("connect", function () {
+
+            node.log("Connected to MQTT Broker: " + node.serverUrl);
+            node.status({fill: "green", shape: "dot", text: "Connected"});
+
+            client.subscribe(node.topic, function (err) {
+                if (!err) {
+                    node.log(`Subscribed to topic: ${node.topic}`);
+                } else {
+                    node.error("Subscription error: " + err.message);
+                }
+            });
+
+        });
+
+        client.on("message", function (topic, message) {
+            node.log(`Received message on ${topic}: ${message.toString()}`);
+            node.send({payload: message.toString(), topic: topic});
+        });
+
+        client.on("error", function (error) {
+            node.error("MQTT Connection Error: " + error.message);
+        });
+
+    }
+
+    function fetchFile(uri) {
+        return new Promise((resolve, reject) => {
+            fs.readdir(uri, (err, files) => {
+                if (err || files.length === 0) {
+                    reject(new Error("No saved file found"));
+                    return;
+                }
+
+                const latestFile = files[0];
+                const filePath = path.join(uri, latestFile); // Get the first file
+                const fileType = mime.lookup(latestFile); // Get MIME type
+
+                fs.readFile(filePath, "utf8", (err, data) => {
+                    if (err) {
+                        reject(new Error('Could not read file'));
+                        return;
+                    }
+                    resolve({
+                        fileContent: data,
+                        fileName: files[0],
+                        fileType: fileType
+                    });
+                });
+            });
+        });
+    }
+
+    function getFilePath(nodeId) {
+        const userDir = RED.settings.userDir;
+        const projects = RED.settings.get("projects");
+
+        const basePath = projects?.activeProject
+            ? path.join(userDir, "projects", projects.activeProject, "uploads")
+            : path.join(userDir, "uploads");
+
+        return path.join(basePath, nodeId);
+    }
+
+    return {
+        getParsedAsyncApiFile,
+        connectToServer,
+        fetchFile,
+        getFilePath
+    }
+};
